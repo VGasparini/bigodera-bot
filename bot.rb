3bf02f433d633cf1616129615a8236795aa03f5b
Vinicius Gasparini <viniciuszeiko@gmail.com>
2018-11-13 19:00:35 -0200

    changed to if stats

--- bot.rb
+++ bot.rb
@@ -1,43 +1,36 @@
 require 'telegram_bot'
 
-token = 
+token = ''
 
 bot = TelegramBot.new(token: token)
 
 bot.get_updates(fail_silently: true) do |message|
-	puts "@#{message.from.username}: #{message.text}"
+	puts "@#{message.from.first_name}: #{message.text}"
 	command = message.get_command_for(bot)
-
-	message.reply do |reply|
-		case command
-		when /start/i
+	message.reply do |reply|		
+		if command == '/start'
 			reply.text = "Bigodera acordado! Agora você já pode utilizar alguns de meus comando. Tente por /help"
-
-		when /help/i
+		elsif (command =~ /\d/)+1
+			reply.text = "Coisa de comunista isso ai, ta ok" 
+		elsif command == '/help/i'
 			reply.text = "Tenho as seguintes funcionalidade\n /start - Me acorda caso esteja dormindo\n /greet - Recepção\n /Weiss - Apenas a verdade"
-
-		when /greet/i
+		elsif command == '/greet/i'
 			saudacao = ['e ai', 'opa', 'olá', 'oie', 'turu bom']
 			final = ['pro URI', 'pro RU', 'pro codeforces', 'pro code', 'pra maratona']
 			reply.text = "#{saudacao.sample.capitalize} #{message.from.first_name}, bem vindo ao BRUTE. Eu sou o Bigodera, o bot dessa galera. Bora #{final.sample}!"
-
-		when /weiss/i
-			wes = ['o caga pau', 'vermelho', 'socável']
+		elsif command == '/weiss/i'
+			wes = ['o caga pau', 'vermelho', 'socável', 'campeão sul brasileiro']
 			reply.text = "#{wes.sample.capitalize}"
-
-		when /jonck/i
-			jnk = ['o engraçado', 'humilde', 'o cara que anima o time']
+		elsif command == '/jonck/i'
+			jnk = ['o engraçado', 'humilde', 'o cara que anima o time', 'campeão sul brasileiro']
 			reply.text = "#{jnk.sample.capitalize}"
-
-		when /felipera/i
-			felipe = ['o hacker', 'carregou mais que Noé', 'cadê meu vinho?']
+		elsif command == '/felipera/i'
+			felipe = ['o rápido', 'carregou mais que Noé', 'cadê meu vinho?', 'campeão sul brasileiro']
 			reply.text = "#{felipe.sample.capitalize}"
-		
 		else
 			reply.text = "Eu não faço ideia do que significa #{command.inspect}.\nFala com o Gasparini se acha que aconteceu algum erro ou uma função massa de ter."
-		
 		end
-		puts "sending #{reply.text.inspect} to @#{message.from.username}"
+		puts "sending #{reply.text.inspect} to @#{message.from.first_name}"
 		reply.send_with(bot)
 	end
 end
